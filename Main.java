7d36a350a5f89d3bb6ec45eba8bfddd8259f427c
AkshataHasabi <akshuh818@gmail.com>
2022-02-21 15:35:21 +0530

    ADDED:created linkedlist of 56>30>70

--- Main.java
+++ Main.java
@@ -4,5 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to linkedlist programms");
+		 MyLinkedList<Integer>  myLinkedList = new  MyLinkedList<>();
+		 MyNode<Integer> nodeOne = new MyNode<>(56);
+		 MyNode<Integer> nodeTwo = new MyNode<>(30);
+		 MyNode<Integer> nodeThree = new MyNode<>(70);
+		 
+		 nodeOne.setNext(nodeTwo);
+		 nodeTwo.setNext(nodeThree);
+		 System.out.println(nodeOne);
 	}
 }
---
+++ MyLinkedList.java
@@ -0,0 +1,15 @@
+package com.bridgelab.linkedlist;
+
+public class MyLinkedList<K> {
+	public INode<K> head;
+	public INode<K> tail;
+	
+	public MyLinkedList() {
+		this.head=null;
+		this.tail=null;
+	}
+	
+	public void printMyNodes() {
+		System.out.println("My Nodes:"+head);
+	}
+}
